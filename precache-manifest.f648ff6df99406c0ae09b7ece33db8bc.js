9b25fb23f3e9f6fd08e6ab112b5a8050a2a273ce
Code Sandbox <hello@codesandbox.io>
2021-03-29 21:05:20 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f648ff6df99406c0ae09b7ece33db8bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cfdccabea410cac4573d72677e2102",
+    "url": "https://dracero.github.io/csb-z4qsb/index.html"
+  },
+  {
+    "revision": "d5b1f61ee45ee296de67",
+    "url": "https://dracero.github.io/csb-z4qsb/static/css/2.1ee0ebfb.chunk.css"
+  },
+  {
+    "revision": "a89468cf6b714b38cd9b",
+    "url": "https://dracero.github.io/csb-z4qsb/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "d5b1f61ee45ee296de67",
+    "url": "https://dracero.github.io/csb-z4qsb/static/js/2.29f912ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://dracero.github.io/csb-z4qsb/static/js/2.29f912ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89468cf6b714b38cd9b",
+    "url": "https://dracero.github.io/csb-z4qsb/static/js/main.a732a743.chunk.js"
+  },
+  {
+    "revision": "0e66053c595d3e5201fe",
+    "url": "https://dracero.github.io/csb-z4qsb/static/js/runtime-main.23452ef6.js"
+  }
+]);
